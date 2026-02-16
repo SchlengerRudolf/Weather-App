b3ea5b75ee78d22fc2f0e850b1f5c5a84f287fdf
Rudolf Schlenger <schlengerrudolfde@gmail.com>
2026-02-16 15:39:04 +0100

    Add loading blur while fetching data

--- src/script/sidebar.js
+++ src/script/sidebar.js
@@ -1,19 +1,24 @@
 import { mainContent } from "./mainContent.js";
 import { weatherData } from "./weatherData.js";
 
+const contentContainer = document.getElementById("main-content");
 const searchbar = document.getElementById("searchbar");
 const searchButton = document.getElementById("searchButton");
 const toggleButton = document.getElementById("toggleButton");
 
 searchButton.addEventListener("click", async () => {
+  contentContainer.className = "loading";
   await weatherData.setData(searchbar.value);
   mainContent.displayContent();
+  contentContainer.className = "";
   searchbar.value = "";
 });
 
 toggleButton.addEventListener("click", async () => {
+  contentContainer.className = "loading";
   await weatherData.toggleUnitGroup();
   mainContent.displayContent();
+  contentContainer.className = "";
   toggleButton.textContent =
     toggleButton.textContent === "Switch to Fahrenheit"
       ? "Switch to Celsius"
